60dfe253ba499fea3b985239bbf6406e9ae0b78a
Nguyen Pham <unknownuser2041@gmail.com>
2024-03-24 02:10:20 -0400

    commit

--- src/main/java/com/example/wordle/HelloApplication.java
+++ src/main/java/com/example/wordle/HelloApplication.java
@@ -5,13 +5,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.*;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws Exception{
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module com.example.wordle {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens com.example.wordle to javafx.fxml;
     exports com.example.wordle;
 }

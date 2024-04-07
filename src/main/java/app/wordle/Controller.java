3a22b68279e35f02774bbd4402f1db6c64841e36
Nguyen Pham <unknownuser2041@gmail.com>
2024-04-07 00:28:51 -0400

    commit major update: spaghetti code edition don't know what the doing right now commit cause depleted brain cells need sleep

--- src/main/java/app/wordle/Controller.java
+++ src/main/java/app/wordle/Controller.java
@@ -2,16 +2,33 @@ package app.wordle;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.GridPane;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 
 import java.io.*;
 import java.util.*;
 
 public class Controller {
-    public static ArrayList<String> dictionary = new ArrayList<String>();
-    public static ArrayList<String> guessedWords = new ArrayList<String>();
-    public static String correctWord;
-    public void getWords() {
+    @FXML public Label debugText;
+    @FXML protected void debugButton() {
+        getWord();
+        debugText.setText("correctword: " + correctWord);
+    }
+    @FXML public GridPane tileGrid;
+    @FXML public GridPane keyboard;
+
+    private ArrayList<String> dictionary = new ArrayList<String>();
+    private ArrayList<String> guessedWords = new ArrayList<String>();
+    private String correctWord;
+
+    private int rows = 5;
+    private int columns = 6;
+
+    public void loadDictonary() {
         try {
             Scanner scanner = new Scanner(new File("src/main/resources/app/wordle/dictionary.txt"));
             while (scanner.hasNextLine()) dictionary.add(scanner.next());
@@ -20,13 +37,41 @@ public class Controller {
             e.printStackTrace();
             System.exit(0);
         }
-        correctWord = dictionary.get((int) (Math.random() * (dictionary.size() - 1)));
-        debugText.setText("correctword: " + correctWord);
     }
+
+    public void getWord() {
+        correctWord = dictionary.get((int) (Math.random() * (dictionary.size() - 1)));
+    }
+
+
+    public void loadTileGrid () {
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= columns; j++) {
+                Label tile = new Label();
+                // tile.getStyleClass().add("");
+                tileGrid.add(tile, j, i); // can use as id
+            }
+        }
+    }
+
+    public void loadKeyboard() {
+
+    }
+
+    @FXML protected void keyboardInput(KeyEvent keyEvent) {
+        if (keyEvent.getCode().isLetterKey()) {
+            // letterInput()
+        } else if (keyEvent.getCode() == KeyCode.BACK_SPACE) {
+            // backspace()
+        } else if (keyEvent.getCode() == KeyCode.ENTER) {
+            // enter()
+        }
+    }
+
     public String validWord(String inputWord) {
         if (inputWord.length() != 5)
             return "word is not a 5 letter word";
-        else if (!dictionary.contains(inputWord))
+        else if (!dictionary.contains(inputWord)) // better word search algorithm
             return "word is not in dictionary";
         else if (guessedWords.contains(inputWord))
             return "word is already tried";
@@ -45,10 +90,7 @@ public class Controller {
         }
     }
 
-    @FXML
-    private Label debugText;
-    @FXML
-    protected void debugButton() {
-        getWords();
+    public void reset() {
+
     }
 }
--- src/main/java/app/wordle/Main.java
+++ src/main/java/app/wordle/Main.java
@@ -5,23 +5,30 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-
-import java.io.*;
+import javafx.stage.Screen;
+import java.io.IOException;
 
 public class Main extends Application {
+    private final Controller controller = new Controller();
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 520, 740);
         scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
 
         stage.getIcons().add(new Image(getClass().getResourceAsStream("icon.png")));
+        stage.setResizable(false);
         stage.setTitle("wordleFX");
         stage.setScene(scene);
         stage.show();
+
+        controller.loadDictonary();
+        controller.loadTileGrid();
+        controller.loadKeyboard();
     }
 
     public static void main(String[] args) {
-        launch(args);
+        launch();
     }
 }

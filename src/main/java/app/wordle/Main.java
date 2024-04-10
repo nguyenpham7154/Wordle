05028a6dea3e9f2a7f8d3aaae0c10f4fe311eee7
Nguyen Pham <unknownuser2041@gmail.com>
2024-04-09 23:24:35 -0400

    commit

--- src/main/java/app/wordle/Main.java
+++ src/main/java/app/wordle/Main.java
@@ -9,12 +9,12 @@ import javafx.stage.Screen;
 import java.io.IOException;
 
 public class Main extends Application {
-    private final Controller controller = new Controller();
+    Controller controller = new Controller();
 
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 520, 740);
+        Scene scene = new Scene(fxmlLoader.load(), 620, 780);
         scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
 
         stage.getIcons().add(new Image(getClass().getResourceAsStream("icon.png")));
@@ -23,9 +23,14 @@ public class Main extends Application {
         stage.setScene(scene);
         stage.show();
 
+        startGame();
+    }
+
+    public void startGame() {
         controller.loadDictonary();
         controller.loadTileGrid();
         controller.loadKeyboard();
+        controller.getWord();
     }
 
     public static void main(String[] args) {

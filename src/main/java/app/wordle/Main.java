61df77c318a647ccfc29953580765b8e9ba6d79f
Nguyen Pham <unknownuser2041@gmail.com>
2024-04-13 14:53:33 -0400

    commit

--- src/main/java/app/wordle/Main.java
+++ src/main/java/app/wordle/Main.java
@@ -3,6 +3,7 @@ package app.wordle;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.Parent;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.Screen;
@@ -11,15 +12,21 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 620, 780);
-        Controller controller = fxmlLoader.getController();
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource("view.fxml"));
+        Parent root = loader.load();
+        Controller controller = loader.getController();
+        Scene scene = new Scene(root, 620, 840);
         scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
 
         stage.getIcons().add(new Image(getClass().getResourceAsStream("icon.png")));
         stage.setResizable(false);
         stage.setTitle("wordleFX");
         stage.setScene(scene);
+
+        controller.loadDictonary();
+        controller.loadKeyboard();
+        controller.loadTileGrid();
+        controller.getWord();
         stage.show();
     }
 

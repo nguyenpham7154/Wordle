3be1043c7e23ead6fabd33cef1cffda431e70f0a
Nguyen Pham <unknownuser2041@gmail.com>
2024-04-13 14:12:23 -0400

    commit Dinh pls check

--- src/main/java/app/wordle/Main.java
+++ src/main/java/app/wordle/Main.java
@@ -13,6 +13,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 620, 780);
+        Controller controller = fxmlLoader.getController();
         scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
 
         stage.getIcons().add(new Image(getClass().getResourceAsStream("icon.png")));
